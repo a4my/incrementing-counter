cab994c118bce004e05806915e088b07f9a5ed3f
Alex Fourmy <alexfourmy@gmail.com>
2021-03-22 13:20:06 +0100

    project is complete

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const counters = document.querySelectorAll('.counter')
 
 counters.forEach(counter => { 
-    counter.innerTex = '0'
+    counter.innerText = '0'
 
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target')
-        const c = +counter.innerTex
+        const c = +counter.innerText
 
         const increment = target / 200
 
         if(c < target) {
-            counter.innerTex = `${Math.ceil(c + increment)}`
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
         }
     }
 
